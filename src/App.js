1a4d14db28b02ea4b67a820b898d27454c74c3e5
dungthai85 <39922626+dungthai85@users.noreply.github.com>
2021-03-01 14:32:43 -0800

    editing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './components/topics/Home';
 import AddPost from './components/topics/AddTopic';
+import Comment from './components/topics/Comment';
 import NavBar from './components/navbar';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -19,7 +20,8 @@ class App extends React.Component {
         <Switch>
         <Redirect exact from="/" to="/Home" />
           <Route path="/Home" component={Home} exact />
-          <Route path="/AddPost" component={AddPost} exact />
+          <Route path="/AddPost" component={AddPost} />
+          <Route path="/Comment" component={Comment} />
           </Switch>
 
       </div>
